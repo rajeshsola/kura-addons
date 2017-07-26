9a37f6d692bdd224f51480a839c1ec70dddcde78
Rajesh Sola <rajeshsola@gmail.com>
2017-07-26 13:09:36 +0530

    Update SocketCanServiceImpl.java

--- socketcan/org.eclipse.kura.protocol.canbus/src/org/eclipse/kura/protocol/canbus/SocketCanServiceImpl.java
+++ socketcan/org.eclipse.kura.protocol.canbus/src/org/eclipse/kura/protocol/canbus/SocketCanServiceImpl.java
@@ -17,26 +17,27 @@ import de.entropia.can.CanSocket.CanId;
 import de.entropia.can.CanSocket.CanInterface;
 import de.entropia.can.CanSocket.Mode;
 
-public class SocketCanServiceImpl implements SocketCanService {
+public class SocketCanServiceImpl implements SocketCanService 
+{
 	private static final Logger s_logger = LoggerFactory.getLogger(SocketCanServiceImpl.class);
 	private static final String APP_ID = "org.eclipse.kura.protocol.canbus";
 	private ComponentContext m_ctx;
 	
-    protected void activate(ComponentContext componentContext) {
-        s_logger.info("activating CanConnectionService");
+	protected void activate(ComponentContext componentContext) {
+     		s_logger.info("activating CanConnectionService");
 		m_ctx=componentContext;		
-    }
+    	}
 
-    protected void deactivate(ComponentContext componentContext) {
+    	protected void deactivate(ComponentContext componentContext) {
     	
-    }
-    /*protected void updated(Map<String, Object> properties)
-    {
+    	}
+   	/*protected void updated(Map<String, Object> properties)
+    	{
     	
-    }*/
-    public SocketCanAgent getSocketCanAgent(String ifName) throws KuraException, IOException
-    {
-    	return new SocketCanAgent(ifName,true);
-    }  
+    	}*/
+    	public SocketCanAgent getSocketCanAgent(String ifName) throws KuraException, IOException
+    	{
+    		return new SocketCanAgent(ifName,true);
+    	}  
     
 }
