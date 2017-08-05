4e2afc53fe777fda0bdb92ac41f914de9b2bc573
Vibhor Khurana <vibhorkhurana@gmail.com>
2017-08-05 14:20:14 +0530

    Changed

--- org.eclipse.kura.tsdb.influxdb/src/org/eclipse/kura/tsdb/influxdb/InfluxService.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.kura.tsdb.influxdb;
-
-import org.influxdb.dto.Pong;
-
-public interface InfluxService {
-	public InfluxDbClient getInfluxDbClient(String host,int port);
-		
-}
-	
